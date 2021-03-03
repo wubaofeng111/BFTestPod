ad859ea0c694bc6c9bc5c07abcb602527dcfcf1c
吴宝锋 <wubaofeng@hudunsoft.com>
2021-03-03 10:43:22 +0800

    first commit

---
+++ BFTestPod/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BFTestPod
+//
+//  Created by zy on 2021/3/3.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
